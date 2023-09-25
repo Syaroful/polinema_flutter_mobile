f0261b944accbddd43e966b0977b2bb441ec961d
Syaroful <syaroful10403@gmail.com>
2023-09-25 14:22:41 +0700

    praktikum 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:polinema_flutter_mobile/page/home_page.dart';
+import 'package:polinema_flutter_mobile/basic_widget/cupertino.dart';
+import 'package:polinema_flutter_mobile/basic_widget/image_widget.dart';
+import 'package:polinema_flutter_mobile/basic_widget/text_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,17 +10,41 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Praktikum 2.7',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.blue,
+        child: const Icon(Icons.thumb_up),
+      ),
+      appBar: AppBar(
+        title: const Text('Flutter Demo'),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CupertinoButtonApp(),
+          ],
+        ),
+      ),
     );
   }
 }
