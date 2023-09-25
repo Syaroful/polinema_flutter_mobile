31faa9b2c3dabef5dd5b13f67e9f3f58ee6456a1
Syaroful <syaroful10403@gmail.com>
2023-09-25 19:38:34 +0700

    initial commit ✅

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:polinema_flutter_mobile/page/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Praktikum 2.7',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Demo'),
+      ),
+      body: const Center(
+        child: Text(
+          'Hello, World!',
+          style: TextStyle(fontSize: 24.0),
+        ),
+      ),
     );
   }
 }
