57a73de0cbd0554a7515af5838979e8b0255a605
Syaroful <syaroful10403@gmail.com>
2023-10-20 13:50:26 +0700

    gak tau

--- lib/main.dart
+++ lib/main.dart
@@ -1,114 +1,124 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Column _buildButtonColumn(Color color, IconData icon, String label) {
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: color),
+          Container(
+            margin: const EdgeInsets.only(top: 8),
+            child: Text(
+              label,
+              style: const TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+
+    Color color = Colors.red;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /* soal 1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /* soal 2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Wisata Gunung di Batu',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Batu, Malang, Indonesia',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /* soal 3*/
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('4.1'),
+        ],
+      ),
+    );
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: const Text(
+        'Pengunjung dapat menikmati trekking yang menantang untuk mencapai '
+        'puncak gunung yang menawarkan panorama spektakuler. Selain itu, '
+        'Gunung Batu juga terkenal dengan berbagai spot fotogenik '
+        'yang menghadirkan kesempatan untuk mengabadikan momen berharga. '
+        'Terdapat pula air terjun, sungai, dan kebun teh '
+        'yang dapat dieksplorasi oleh pengunjung. '
+        'Muhamad Syarful Anam - 2141720108',
+        softWrap: true,
+        style: TextStyle(fontSize: 16),
+      ),
+    );
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Syaroful Anam - 2141720108',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(children: [
-        Container(
-          height: 280,
-          color: Colors.red,
-          child: Image.network(
-            'https://images.unsplash.com/photo-1610809027249-86c649feacd5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
-            fit: BoxFit.cover,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Mountain', textAlign: TextAlign.center),
+          centerTitle: true,
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {},
           ),
         ),
-        const Padding(
-          padding: EdgeInsets.all(20.0),
-          child: Row(
-            children: [
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Gunung Kembar',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      'switzerland',
-                      style: TextStyle(fontSize: 20, color: Colors.grey),
-                    ),
-                  ],
-                ),
+        body: Column(
+          children: [
+            Container(
+              height: 280,
+              color: Colors.red,
+              child: Image.network(
+                'https://images.unsplash.com/photo-1610809027249-86c649feacd5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
+                fit: BoxFit.cover,
               ),
-              Icon(
-                Icons.star,
-                color: Colors.red,
-              ),
-              Text('5.0'),
-            ],
-          ),
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
         ),
-        const Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Column(
-                children: [
-                  Icon(
-                    Icons.call,
-                    color: Colors.red,
-                    size: 30,
-                  ),
-                  Text('CALL'),
-                ],
-              ),
-              Column(
-                children: [
-                  Icon(
-                    Icons.location_on,
-                    color: Colors.red,
-                    size: 30,
-                  ),
-                  Text('ROUTE'),
-                ],
-              ),
-              Column(
-                children: [
-                  Icon(
-                    Icons.share,
-                    color: Colors.red,
-                    size: 30,
-                  ),
-                  Text('SHARE'),
-                ],
-              ),
-            ],
-          ),
-        ),
-        const Padding(
-          padding: EdgeInsets.all(20),
-          child: Text(
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lobortis augue a ligula pellentesque rutrum. Sed sed eros risus. Proin quam diam, commodo finibus est nec, feugiat blandit neque. In venenatis viverra mattis. Curabitur at felis vehicula, aliquam nibh a, ornare sapien. Aliquam consectetur mauris libero, id efficitur augue luctus sit amet. Pellentesque tristique libero ut mauris bibendum dignissim. Sed sodales nisi nec felis viverra, ut cursus risus scelerisque. Nunc dapibus dapibus ex et vulputate.'),
-        )
-      ]),
+      ),
     );
   }
 }
